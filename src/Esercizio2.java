e2c20f06fe5be86e024fbee13247944839d573f2
MaurizioCr <crispinomaurizio034@gmail.com>
2023-12-05 16:35:06 +0100

    Pari dispari

--- src/Esercizio2.java
+++ src/Esercizio2.java
@@ -3,16 +3,18 @@ import java.util.*;
 public class Esercizio2 {
     public static void main(String[] args) {
         int n = 0;
-        int count=0;
         boolean inputValido = false;
+        boolean pari;
         //Per numero casuale
         Random random = new Random();
 
-        List randomNumber = new ArrayList();
+        List <Integer> randomNumber = new ArrayList();
+        List <Integer> numeriPari = new ArrayList();
+        List <Integer> numeriDispari = new ArrayList();
         Scanner scanner = new Scanner(System.in);
         do {
             try {
-                //Ottengo il numero dall'utente
+                //Ottengo il numero sentential
                 System.out.println("Inserisci il numero di di valori casuali da generare nella tua lista ");
                 n = scanner.nextInt();
                 scanner.nextLine();
@@ -37,10 +39,30 @@ public class Esercizio2 {
             int numeroCasuale = random.nextInt(100) + 1;
             randomNumber.add(numeroCasuale);
         }
-
-        Collections.sort(randomNumber);
         scanner.close();
-        System.out.println(randomNumber);
+
+        //Ordinare la lista
+        Collections.sort(randomNumber);
+        System.out.println("List ordinata: " + randomNumber);
+
+        //Invertire la lista
+        Collections.sort(randomNumber, Collections.reverseOrder());
+        System.out.println("List non ordinata: " + randomNumber);
+        System.out.println(randomNumber.get(0));
+
+        //Nuova lista di numeri pari
+        for (int i=0; i<randomNumber.size(); i++){
+
+            if (randomNumber.get(i)%2==0){
+                numeriPari.add(randomNumber.get(i));
+            }
+            else{
+                numeriDispari.add(randomNumber.get(i));
+            }
+        }
+        System.out.println("Numeri pari della lista: "+numeriPari);
+        System.out.println("Numeri pari della lista: "+numeriDispari);
+
 
     }
 }
