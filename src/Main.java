da195880df8a3c0d8341b0642360b311e3c097dc
MaurizioCr <crispinomaurizio034@gmail.com>
2023-12-05 15:23:44 +0100

    Fix try catch e commenti

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,51 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n=0;
         int count=0;
+        boolean inputValido=false;
         String parolaAggiunta;
+
+        //Creazione lista parola
         HashSet<String> parole = new HashSet<>();
+
+        //Creazione lista parola duplicate
         HashSet<String> duplicati = new HashSet<>();
 
-        try {
-            System.out.println("Inserisci il numero di parole da inserire nella lista");
-            n = scanner.nextInt();
-            scanner.nextLine();
-        }
-        catch (InputMismatchException ex){
-            System.out.println("Inserisci un valore positivo");
-        }
+        do {
+            try {
+                //Ottengo il numero dall'utente
+                System.out.println("Inserisci il numero di parole da inserire nella lista");
+                n = scanner.nextInt();
+                scanner.nextLine();
 
+                //In caso di numero positivo input true per uscire dal loop
+                if (n > 0) {
+                    inputValido = true;
+                } else {
+                    System.err.println("Inserisci un valore intero positivo.");
+                    scanner.nextLine();
+                }
+
+                //Catturare Errore non valore
+            } catch (InputMismatchException ex) {
+                System.err.println("Inserisci un valore intero positivo.");
+                scanner.nextLine();
+            }
+            //Finchè l'input è false ripetere il loop
+        } while (!inputValido);
 
         do {
             System.out.println("Scrivi le parole da inserire");
             parolaAggiunta= scanner.nextLine();
+
+            //Se la parola non viene aggiunta alla lista parole verrà aggiunta alla lista duplicati
             if (!parole.add(parolaAggiunta)) {
                 duplicati.add(parolaAggiunta);
             }
             count++;
         } while (count!=n);
         scanner.close();
+
+                        //For each delle liste
 
         System.out.println("*****ECCO LA TUA LISTA*****");
         for (String parola:parole
